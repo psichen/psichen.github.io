e06683ff5add5a22651c988c6904f71fb466a2ba
psichen <psichen@outlook.com>
2019-09-27 08:15:37 +0800

    sm.ms

--- script/myFunction.js
+++
@@ -1,4 +0,0 @@
-hexo.extend.tag.register('img', function(args){
-    const image_url = args[0], alt = args[1];
-    return `<img src="${image_url}" alt="${alt}" referrerpolicy="no-referrer"></img>`
-})
